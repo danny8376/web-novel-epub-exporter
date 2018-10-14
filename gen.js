b19afd5bb914df8c94d74e5c7b3df8dad737f036
DannyAAM <danny@saru.moe>
2018-10-14 08:43:43 +0800

    add generation of index file

--- gen.js
+++ gen.js
@@ -5,7 +5,7 @@ async function main() {
     var config = JSON.parse(fs.readFileSync(process.argv[2]));
     var gitbook = new Gitbook(config.book_uri, config.options);
     await gitbook.ready();
-    gitbook.gen(true, true);
-    gitbook.autoRegen(true, true);
+    gitbook.gen(true, true, true);
+    gitbook.autoRegen(true, true, true);
 }
 main();
